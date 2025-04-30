8b1deea1a307738209a8fa005c3495151049b1e1
Emdya Permuy <67919243+Emdya@users.noreply.github.com>
2025-04-30 10:16:24 -0400

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,43 +1,36 @@
-const SkeletonOrderItems = () => {
+import SkeletonCartTotals from "@modules/skeletons/components/skeleton-cart-totals"
+
+const SkeletonOrderInformation = () => {
   return (
-    <div className="flex flex-col gap-y-4 py-10 border-y border-gray-200">
-      <div className="grid grid-cols-[122px_1fr] gap-x-4">
-        <div className="w-full aspect-[29/34] bg-gray-100"></div>
-        <div className="flex items-start justify-between">
-          <div className="flex flex-col gap-y-2">
-            <div className="w-48 h-6 bg-gray-100"></div>
-            <div className="w-24 h-4 bg-gray-100"></div>
-            <div className="w-32 h-4 bg-gray-100"></div>
-          </div>
-          <div className="w-32 h-6 bg-gray-100"></div>
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 py-10 border-b border-gray-200">
+        <div className="flex flex-col">
+          <div className="w-32 h-4 bg-gray-100 mb-4"></div>
+          <div className="w-2/6 h-3 bg-gray-100"></div>
+          <div className="w-3/6 h-3 bg-gray-100 my-2"></div>
+          <div className="w-1/6 h-3 bg-gray-100"></div>
+        </div>
+        <div className="flex flex-col">
+          <div className="w-32 h-4 bg-gray-100 mb-4"></div>
+          <div className="w-2/6 h-3 bg-gray-100"></div>
+          <div className="w-3/6 h-3 bg-gray-100 my-2"></div>
+          <div className="w-2/6 h-3 bg-gray-100"></div>
+          <div className="w-1/6 h-3 bg-gray-100 mt-2"></div>
+          <div className="w-32 h-4 bg-gray-100 my-4"></div>
+          <div className="w-1/6 h-3 bg-gray-100"></div>
         </div>
       </div>
-
-      <div className="grid grid-cols-[122px_1fr] gap-x-4">
-        <div className="w-full aspect-[29/34] bg-gray-100"></div>
-        <div className="flex items-start justify-between">
-          <div className="flex flex-col gap-y-2">
-            <div className="w-48 h-6 bg-gray-100"></div>
-            <div className="w-24 h-4 bg-gray-100"></div>
-            <div className="w-32 h-4 bg-gray-100"></div>
-          </div>
-          <div className="w-32 h-6 bg-gray-100"></div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 py-10">
+        <div className="flex flex-col">
+          <div className="w-32 h-4 bg-gray-100 mb-4"></div>
+          <div className="w-2/6 h-3 bg-gray-100"></div>
+          <div className="w-3/6 h-3 bg-gray-100 my-4"></div>
         </div>
-      </div>
 
-      <div className="grid grid-cols-[122px_1fr] gap-x-4">
-        <div className="w-full aspect-[29/34] bg-gray-100"></div>
-        <div className="flex items-start justify-between">
-          <div className="flex flex-col gap-y-2">
-            <div className="w-48 h-6 bg-gray-100"></div>
-            <div className="w-24 h-4 bg-gray-100"></div>
-            <div className="w-32 h-4 bg-gray-100"></div>
-          </div>
-          <div className="w-32 h-6 bg-gray-100"></div>
-        </div>
+        <SkeletonCartTotals />
       </div>
     </div>
   )
 }
 
-export default SkeletonOrderItems
+export default SkeletonOrderInformation
