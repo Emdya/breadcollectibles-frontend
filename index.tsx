b718c36f3900c3bb6cca0548fb5c8f7fd6d921b7
Emdya Permuy <67919243+Emdya@users.noreply.github.com>
2025-04-30 10:16:13 -0400

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,15 +1,43 @@
-import SkeletonButton from "@modules/skeletons/components/skeleton-button"
-import SkeletonCartTotals from "@modules/skeletons/components/skeleton-cart-totals"
-
-const SkeletonOrderSummary = () => {
+const SkeletonOrderItems = () => {
   return (
-    <div className="grid-cols-1">
-      <SkeletonCartTotals header={false} />
-      <div className="mt-4">
-        <SkeletonButton />
+    <div className="flex flex-col gap-y-4 py-10 border-y border-gray-200">
+      <div className="grid grid-cols-[122px_1fr] gap-x-4">
+        <div className="w-full aspect-[29/34] bg-gray-100"></div>
+        <div className="flex items-start justify-between">
+          <div className="flex flex-col gap-y-2">
+            <div className="w-48 h-6 bg-gray-100"></div>
+            <div className="w-24 h-4 bg-gray-100"></div>
+            <div className="w-32 h-4 bg-gray-100"></div>
+          </div>
+          <div className="w-32 h-6 bg-gray-100"></div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-[122px_1fr] gap-x-4">
+        <div className="w-full aspect-[29/34] bg-gray-100"></div>
+        <div className="flex items-start justify-between">
+          <div className="flex flex-col gap-y-2">
+            <div className="w-48 h-6 bg-gray-100"></div>
+            <div className="w-24 h-4 bg-gray-100"></div>
+            <div className="w-32 h-4 bg-gray-100"></div>
+          </div>
+          <div className="w-32 h-6 bg-gray-100"></div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-[122px_1fr] gap-x-4">
+        <div className="w-full aspect-[29/34] bg-gray-100"></div>
+        <div className="flex items-start justify-between">
+          <div className="flex flex-col gap-y-2">
+            <div className="w-48 h-6 bg-gray-100"></div>
+            <div className="w-24 h-4 bg-gray-100"></div>
+            <div className="w-32 h-4 bg-gray-100"></div>
+          </div>
+          <div className="w-32 h-6 bg-gray-100"></div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default SkeletonOrderSummary
+export default SkeletonOrderItems
